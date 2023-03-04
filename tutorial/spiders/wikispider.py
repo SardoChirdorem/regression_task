1a2716ba6d869be5d4a2a020132542d0ea88334d
Siddharth <mrdas185@gmail.com>
2023-03-05 01:20:08 +0530

    Oi Oi just committing

---
+++ tutorial/spiders/wikispider.py
@@ -0,0 +1,24 @@
+import scrapy
+import os
+
+
+class wikispider(scrapy.Spider):
+    name = "wiki"
+
+    def start_requests(self):
+        urls = [
+            "https://en.wikipedia.org/wiki/ABBA"
+        ]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-3]
+        filename = f"{page}.txt"
+        os.remove(r"C:\Users\mrdas\scrapy\tutorial\tutorial\spiders\en.wikipedia.org.txt")
+        for para in response.xpath("//div[@class='mw-parser-output']/p"):
+            text = para.xpath("text()").getall()
+            with open(filename, "a", encoding="utf-8") as f:
+                f.write(str(text) + "\n")
+        self.log(f"Saved file: {filename}")
